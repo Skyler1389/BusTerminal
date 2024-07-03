331516f4661673e3faa2d7669920d13e987f4f92
Skyler <99615429+Skyler1389@users.noreply.github.com>
2024-07-03 11:04:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Projektvorgabe:
+  
+Zwischen zwei Terminals eines Hafens verkehrt ein Zubringerbus, der jeweils
+zur halben und zur vollen Stunde losfährt. Schreiben Sie ein Programm, das
+eine Uhrzeit in der Form hhmm (also ein Parameter, aber ohne führende Nullen)
+erhält und berechnet, wie lange es bis zur Abfahrt des nächsten Busses 
+noch dauert. Dabei bezeichnet hh die aktuelle Stunde und mm die aktuelle Minute.
+Tipp: Berechne bitte aus den Minuten der aktuellen Uhrzeit die zugehörige 
+halbe Stunde.
+
+ */
+
+namespace BusTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Timekeeper.getTimeUntilNextBusDepartures("1337"));
+        }
+    }
+}
---
+++ Timekeeper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace BusTerminal
+{
+    internal static class Timekeeper
+    {
+        public static String getTimeUntilNextBusDepartures(String hhmm)
+        {
+            if (hhmm.Length > 4)
+            {
+                throw new ArgumentException(hhmm 
+                    + " String does not conform to hhmm format!");
+                // Fehlermeldung, falls der String nicht dem Format
+                // entsprechen kann
+            }
+            if (int.Parse(String.Concat(hhmm[0], hhmm[1])) >= 24 
+                || int.Parse(String.Concat(hhmm[2], hhmm[3])) >= 60)
+            {
+                throw new ArgumentException(hhmm 
+                    + " String is not a valid time!");
+                // Fehlermeldung, falls die gegeben Zeit nicht existiert
+            }
+            if (hhmm.Length < 4)
+            {
+                hhmm.PadLeft(-hhmm.Length + 4, '0');
+                // Füge führende Nullen hinzu, wenn die Uhrzeit ohne
+                // übergeben wurde.
+            }
+            int minutes = int.Parse(String.Concat(hhmm[2], hhmm[3]));
+            // Ermittle die Minuten anhand des Übergebenen Strings
+            int minutesSinceLastBiHourlyDeparture = minutes % 30;
+            // Ermittle Unterschied der aktuellen Minute zur letzten
+            // halben Stunde
+            int minutesUntilNextBiHourlyDeparture = 30 
+                - minutesSinceLastBiHourlyDeparture;
+            // Ermittle die Differenz zwischen der nächsten
+            // und letzten Abfahrt
+            String output = "The next bus departure is in " 
+                + minutesUntilNextBiHourlyDeparture.ToString() + " minutes";
+            if (minutesUntilNextBiHourlyDeparture == 30)
+            {
+                output = "The bus is departing now!";
+            }
+            return output;
+        }
+    }
+}
